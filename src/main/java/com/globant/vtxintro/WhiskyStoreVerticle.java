2fed0621c1fa4dc46a67015f344e93d62cb7f72e
Juan Toro <juan.toro@globant.com>
2017-12-29 16:59:40 -0500

    Avances

--- src/main/java/com/globant/vtxintro/WhiskyStoreVerticle.java
+++ src/main/java/com/globant/vtxintro/WhiskyStoreVerticle.java
@@ -2,9 +2,7 @@ package com.globant.vtxintro;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import com.globant.vtxintro.bo.Whisky;
-
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerResponse;
@@ -16,73 +14,70 @@ import io.vertx.ext.web.handler.StaticHandler;
 
 public class WhiskyStoreVerticle extends AbstractVerticle {
 
-	// Store our product
-	private Map<Integer, Whisky> products = new LinkedHashMap<>();
+  // Store our product
+  private Map<Integer, Whisky> products = new LinkedHashMap<>();
 
-	@Override
-	public void start(Future<Void> fut) {
+  @Override
+  public void start(Future<Void> fut) {
 
-		createSomeData();
+    createSomeData();
 
-		Router router = Router.router(vertx);
+    Router router = Router.router(vertx);
 
-		// Bind "/" to our hello message - so we are still compatible.
-		router.route("/").handler(routingContext -> {
-			HttpServerResponse response = routingContext.response();
-			response.putHeader("content-type", "text/html").end("<h1>Hola!</h1>");
-		});
+    // Bind "/" to our hello message - so we are still compatible.
+    router.route("/").handler(routingContext -> {
+      HttpServerResponse response = routingContext.response();
+      response.putHeader("content-type", "text/html").end("<h1>Hola!</h1>");
+    });
 
-		// Serve static resources from the /assets directory
-		router.route("/assets/*").handler(StaticHandler.create("assets"));
+    // Serve static resources from the /assets directory
+    router.route("/assets/*").handler(StaticHandler.create("assets"));
 
-		router.get("/api/whiskies").handler(this::getAll);
-		router.route("/api/whiskies*").handler(BodyHandler.create());
-		router.post("/api/whiskies").handler(this::addOne);
-		router.delete("/api/whiskies/:id").handler(this::deleteOne);
+    router.get("/api/whiskies").handler(this::getAll);
+    router.route("/api/whiskies*").handler(BodyHandler.create());
+    router.post("/api/whiskies").handler(this::addOne);
+    router.delete("/api/whiskies/:id").handler(this::deleteOne);
 
-		vertx.createHttpServer().requestHandler(router::accept).listen(config().getInteger("http.port", 8080),
-			result -> {
-				if (result.succeeded()) {
-					fut.complete();
-				} else {
-					fut.fail(result.cause());
-				}
-			});
+    vertx.createHttpServer().requestHandler(router::accept)
+        .listen(config().getInteger("http.port", 8080), result -> {
+          if (result.succeeded()) {
+            fut.complete();
+          } else {
+            fut.fail(result.cause());
+          }
+        });
 
-	}
+  }
 
-	// Create some product
-	private void createSomeData() {
-		Whisky bowmore = new Whisky("Bowmore 15 Years Laimrig", "Scotland, Islay");
-		products.put(bowmore.getId(), bowmore);
-		Whisky talisker = new Whisky("Talisker 57° North", "Scotland, Island");
-		products.put(talisker.getId(), talisker);
-	}
+  // Create some product
+  private void createSomeData() {
+    Whisky bowmore = new Whisky("Bowmore 15 Years Laimrig", "Scotland, Islay");
+    products.put(bowmore.getId(), bowmore);
+    Whisky talisker = new Whisky("Talisker 57ï¿½ North", "Scotland, Island");
+    products.put(talisker.getId(), talisker);
+  }
 
-	private void getAll(RoutingContext routingContext) {
-		  routingContext.response()
-		      .putHeader("content-type", "application/json; charset=utf-8")
-		      .end(Json.encodePrettily(products.values()));
-	}
-	
-	private void addOne(RoutingContext routingContext) {
-		  final Whisky whisky = Json.decodeValue(routingContext.getBodyAsString(),
-		      Whisky.class);
-		  products.put(whisky.getId(), whisky);
-		  routingContext.response()
-		      .setStatusCode(201)
-		      .putHeader("content-type", "application/json; charset=utf-8")
-		      .end(Json.encodePrettily(whisky));
-	}
-	
-	private void deleteOne(RoutingContext routingContext) {
-		  String id = routingContext.request().getParam("id");
-		  if (id == null) {
-		    routingContext.response().setStatusCode(400).end();
-		  } else {
-		    Integer idAsInteger = Integer.valueOf(id);
-		    products.remove(idAsInteger);
-		  }
-		  routingContext.response().setStatusCode(204).end();
-	}
+  private void getAll(RoutingContext routingContext) {
+    routingContext.response().putHeader("content-type", "application/json; charset=utf-8")
+        .end(Json.encodePrettily(products.values()));
+  }
+
+  private void addOne(RoutingContext routingContext) {
+    final Whisky whisky = Json.decodeValue(routingContext.getBodyAsString(), Whisky.class);
+    products.put(whisky.getId(), whisky);
+    routingContext.response().setStatusCode(201)
+        .putHeader("content-type", "application/json; charset=utf-8")
+        .end(Json.encodePrettily(whisky));
+  }
+
+  private void deleteOne(RoutingContext routingContext) {
+    String id = routingContext.request().getParam("id");
+    if (id == null) {
+      routingContext.response().setStatusCode(400).end();
+    } else {
+      Integer idAsInteger = Integer.valueOf(id);
+      products.remove(idAsInteger);
+    }
+    routingContext.response().setStatusCode(204).end();
+  }
 }
---
+++ src/test/java/com/globant/vtxintro/MyFirstVerticleTest.java
@@ -0,0 +1,99 @@
+package com.globant.vtxintro;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import com.globant.vtxintro.WhiskyStoreVerticleEB;
+import com.globant.vtxintro.bo.Whisky;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+@RunWith(VertxUnitRunner.class)
+public class MyFirstVerticleTest {
+
+  private Vertx vertx;
+  private int port;
+
+  @Before
+  public void setUp(TestContext context) {
+    vertx = Vertx.vertx();
+
+    try {
+      ServerSocket socket = new ServerSocket(0);
+      port = socket.getLocalPort();
+      socket.close();
+
+      DeploymentOptions options =
+          new DeploymentOptions().setConfig(new JsonObject().put("http.port", port));
+      vertx.deployVerticle(WhiskyStoreVerticleEB.class.getName(), options);
+      vertx.deployVerticle(FeatureVerticle.class.getName(), options);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @After
+  public void tearDown(TestContext context) {
+    vertx.close(context.asyncAssertSuccess());
+  }
+
+  @Test
+  public void testMyApplication(TestContext context) {
+    final Async async = context.async();
+
+    vertx.createHttpClient().getNow(port, "localhost", "/", response -> {
+      response.handler(body -> {
+        context.assertTrue(body.toString().contains("Hola"));
+        async.complete();
+      });
+    });
+  }
+
+  @Test
+  public void checkThatTheIndexPageIsServed(TestContext context) {
+    Async async = context.async();
+    vertx.createHttpClient().getNow(port, "localhost", "/assets/index.html", response -> {
+
+      context.assertEquals(response.statusCode(), 200);
+      context.assertEquals(response.headers().get("content-type"), "text/html");
+
+      response.bodyHandler(body -> {
+        context.assertTrue(body.toString().contains("<title>My Whisky Collection</title>"));
+        async.complete();
+      });
+    });
+  }
+
+  /**
+   * "Test description"
+   * 
+   * @param context
+   */
+  @Test
+  public void checkThatWeCanAdd(TestContext context) {
+    Async async = context.async();
+    final String json = Json.encodePrettily(new Whisky("Jameson", "Ireland"));
+    final String length = Integer.toString(json.length());
+    vertx.createHttpClient().post(port, "localhost", "/api/whiskies")
+        .putHeader("content-type", "application/json").putHeader("content-length", length)
+        .handler(response -> {
+          context.assertEquals(response.statusCode(), 201);
+          context.assertTrue(response.headers().get("content-type").contains("application/json"));
+          response.bodyHandler(body -> {
+            final Whisky whisky = Json.decodeValue(body.toString(), Whisky.class);
+            context.assertEquals(whisky.getName(), "Jameson");
+            context.assertEquals(whisky.getOrigin(), "Ireland");
+            context.assertNotNull(whisky.getId());
+            async.complete();
+          });
+        }).write(json).end();
+  }
+}
